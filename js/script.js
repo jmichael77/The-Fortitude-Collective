6039fd7cb1856de0e26de77495e3146e917380c8
James Michael <jmichaely2k7@outlook.com>
2020-02-19 12:21:04 +0000

    update studio time btn link

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.parallax').parallax();
-  
+
   //smooth scrolling
   $('a[href*="#"]:not([href="#"])').click(function () {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -14,4 +14,5 @@ $(document).ready(function(){
           }
       }
   });
+
 });
